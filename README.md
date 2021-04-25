d672169ee6d0d0e72d134cabcf661fac10ec0d1e
anhtran17 <anhtranngocy7@gmail.com>
2021-04-25 22:56:41 +0700

    XML signin

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # DoAnKotlin - Mobile
+
+## Lưu ý khi đặt tên 
+
+- Nhom3 + ten rieng + ten chuc nang + Activity - Nhom3AnhSignInActivity
+- Cho các chức năng vào folder riêng ( theo dõi BTVN )
+## Lưu ý khi thiết kế:
+
+- Thiết kế trên điện thoại google pixel 3
+- Màu sắc sẽ lựa chọn theo thiết kế ở figma
---
+++ Source/app/src/main/java/com/laptrinhdidong/nhom3/quanlichitieu/SignIn/Nhom3AnhSignInActivity.kt
@@ -0,0 +1,11 @@
+package com.laptrinhdidong.nhom3.quanlichitieu
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Nhom3AnhSignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.nhom3_anh_activity_sign_in)
+    }
+}
