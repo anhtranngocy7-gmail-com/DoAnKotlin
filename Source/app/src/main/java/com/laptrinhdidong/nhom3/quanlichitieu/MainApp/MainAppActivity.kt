42dcabc016fc2cabcb16b01f8469b7b0b4f8ff87
NguyenQuocAn99 <nqan1999vvk@gmail.com>
2021-05-03 17:41:57 +0700

    tổng quan xml, thống kê xml, menu navigation fragment'

--- Source/app/src/main/java/com/laptrinhdidong/nhom3/quanlichitieu/MainApp/MainAppActivity.kt
+++ Source/app/src/main/java/com/laptrinhdidong/nhom3/quanlichitieu/MainApp/MainAppActivity.kt
@@ -10,21 +10,22 @@ import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.laptrinhdidong.nhom3.quanlichitieu.R
+import com.laptrinhdidong.nhom3.quanlichitieu.Thongke.Nhom3QuocThongKeFragment
 import com.laptrinhdidong.nhom3.quanlichitieu.databinding.ActivityMainAppBinding
 
 class MainAppActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainAppBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=DataBindingUtil.setContentView(this,R.layout.activity_main_app)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main_app)
         binding.bottomNavigationView.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener { item: MenuItem ->
             supportFragmentManager.commit {
                 setReorderingAllowed(true)
 
                 when (item.itemId) {
-                    R.id.action_home -> add<TongQuanFragment>(R.id.fragment_mainapp)
-                    R.id.action_accumulate -> add<TichLuyFragment>(R.id.fragment_mainapp)
-//                    R.id.action_accumulate -> Toast.makeText(this@MainAppActivity,"tichluy",Toast.LENGTH_SHORT).show()
+                    R.id.action_home -> replace<TongQuanFragment>(R.id.fragment_mainapp)
+                    R.id.action_accumulate -> replace<TichLuyFragment>(R.id.fragment_mainapp)
+                    R.id.action_report -> replace<Nhom3QuocThongKeFragment>(R.id.fragment_mainapp)
 
                 }
                 addToBackStack(null)
