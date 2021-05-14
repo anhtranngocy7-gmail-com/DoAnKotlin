d0db4bb3dc5eb0bdd18e57635e30c14795a4bc57
anhtran17 <anhtranngocy7@gmail.com>
2021-05-15 02:07:29 +0700

    button image login fb

--- Source/app/src/main/java/com/laptrinhdidong/nhom3/quanlichitieu/SignIn/Nhom3AnhSignInActivity.kt
+++ Source/app/src/main/java/com/laptrinhdidong/nhom3/quanlichitieu/SignIn/Nhom3AnhSignInActivity.kt
@@ -5,10 +5,12 @@ import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Base64
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.facebook.*
+import com.facebook.FacebookActivity
 import com.facebook.login.LoginResult
 import com.facebook.login.widget.LoginButton
 import com.laptrinhdidong.nhom3.quanlichitieu.SignIn.Nhom3AnhSignInViewModel
@@ -32,15 +34,21 @@ class Nhom3AnhSignInActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this,R.layout.nhom3_anh_activity_sign_in)
         viewModel = ViewModelProvider(this).get(Nhom3AnhSignInViewModel::class.java)
         binding.account = viewModel.account
-
         binding.btnConfirmSignin.setOnClickListener {
             Log.e("Check User", "Waiting check")
         }
         printKeyHash()
+        binding.btnfbSignin.setVisibility(View.GONE);
         binding.btnfbSignin.setReadPermissions(Arrays.asList("public_profile","email"))
+        binding.btnfbSigninImage.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(v: View?) {
+                val btn = LoginButton(this@Nhom3AnhSignInActivity)
+                btn.performClick()
+            }
+
+        })
         setLogin_Button()
     }
-
     private fun setLogin_Button() {
         binding.btnfbSignin.registerCallback(callbackManager, object : FacebookCallback<LoginResult?> {
             override fun onSuccess(loginResult: LoginResult?) {
@@ -51,19 +59,20 @@ class Nhom3AnhSignInActivity : AppCompatActivity() {
             override fun onCancel() {
                 // App code
             }
-
             override fun onError(exception: FacebookException) {
                 // App code
             }
         })
     }
-
     private fun result() {
         val graphRequest = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(), object: GraphRequest.GraphJSONObjectCallback
                 {
                     override fun onCompleted(`object`: JSONObject?, response: GraphResponse?) {
                         if (response != null) {
                             Log.e("JSON",response.jsonObject.toString())
+                            val email = response.jsonObject.getString("email")
+                            val name = response.jsonObject.getString("name")
+                            val idUser = response.jsonObject.getString("id")
                         }
                     }
                 }
@@ -73,12 +82,10 @@ class Nhom3AnhSignInActivity : AppCompatActivity() {
         graphRequest.setParameters(parameter)
         graphRequest.executeAsync()
     }
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         callbackManager.onActivityResult(requestCode, resultCode, data)
     }
-
     private fun printKeyHash()
     {
         try{
