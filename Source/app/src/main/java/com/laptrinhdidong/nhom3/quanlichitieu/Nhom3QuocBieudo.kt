aafd026dded39b37f6552a5694fe07e78c0cb6a5
Nguyen Hoang Quoc <nhquoc99@gmail.com>
2021-05-21 20:27:44 +0700

    Update Bieu do chi tieu Fragment

---
+++ Source/app/src/main/java/com/laptrinhdidong/nhom3/quanlichitieu/Nhom3QuocBieudo.kt
@@ -0,0 +1,38 @@
+package com.laptrinhdidong.nhom3.quanlichitieu
+
+import android.os.Bundle
+
+import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+
+
+class Nhom3QuocBieudo : AppCompatActivity(){
+    private lateinit var toolbar1: Toolbar
+    private lateinit var viewPager1 : ViewPager
+    private lateinit var tabLayout1 : TabLayout
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.nhom3_quoc_activity_bieudo)
+
+        toolbar1 = findViewById(R.id.toolbar)
+        viewPager1 = findViewById(R.id.viewPager)
+        tabLayout1 =findViewById(R.id.tabLayout)
+
+        toolbar1.setTitle("Biểu đồ chi tiêu")
+
+
+
+       val fragmentAdapter = Nhom3QuocFragmentPageAdapter(supportFragmentManager)
+
+        viewPager1.adapter = fragmentAdapter
+
+        tabLayout1.setupWithViewPager(viewPager1)
+
+
+
+    }
+}
