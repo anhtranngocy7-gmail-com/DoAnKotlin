bd5c21454b41b5991275ca7d042a7985ece2be5b
Thanh Binh Le <binh.lethanh1907@gmail.com>
2021-05-24 23:43:12 +0700

    Update Voice Recognizer

--- Source/app/src/main/java/com/laptrinhdidong/nhom3/quanlichitieu/Spending/Nhom3BinhSpendingActivity.kt
+++ Source/app/src/main/java/com/laptrinhdidong/nhom3/quanlichitieu/Spending/Nhom3BinhSpendingActivity.kt
@@ -1,20 +1,74 @@
 package com.laptrinhdidong.nhom3.quanlichitieu.Spending
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.laptrinhdidong.nhom3.quanlichitieu.R
 import com.laptrinhdidong.nhom3.quanlichitieu.databinding.Nhom3BinhActivitySpendingBinding
+import java.util.*
 
 
 class Nhom3BinhSpendingActivity: AppCompatActivity() {
     private lateinit var viewModel: Nhom3BinhSpendingViewModel
     private lateinit var binding: Nhom3BinhActivitySpendingBinding
+    private val REQ_CODE_SPEECH_INPUT = 1 // Kiem tra gia tri tra cua giong noi
+
+
+    var get_string_voice_input = ArrayList<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.nhom3_binh_activity_spending)
         viewModel = ViewModelProvider(this).get(Nhom3BinhSpendingViewModel::class.java)
         binding.calendar.append(viewModel.sdf.format(viewModel.date))
+        binding.micro.setOnClickListener(
+            View.OnClickListener
+// Phương thức lấy tác động khi nhấn vào button
+            { promptSpeechInput() })
+    }
+    private fun promptSpeechInput() {
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        //-	Bắt đầu một hoạt động sẽ nhắc người dùng về giọng nói và gửi nó qua quá trình nhận dạng,
+        // sau đó hiển thị kết quả trên web hoặc kích hoạt một loại hoạt động dựa trên giọng nói của người dùng.
+        // Xác nhận ứng dụng muốn gửi yêu cầu.
+        intent.putExtra(
+            RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+        )
+        // Gợi ý nhận dạng những gì người dùng nói
+        /**intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault()); */
+        intent.putExtra(
+            RecognizerIntent.EXTRA_PROMPT,
+            getString(R.string.speech_prompt)
+        )
+        // Hiển thị yêu cầu cho người dùng chuẩn bị nói vào
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "vi-VN")
+        // Chọn ngôn ngữ nói vào: ở đây chọn tiếng Việt
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT)
+        } catch (a: ActivityNotFoundException) {
+            Toast.makeText(
+                getApplicationContext(),
+                getString(R.string.speech_not_supported),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        // Nếu không tìm thấy hoạt động của intent, hiển thị ra màn hình 3s dòng chữ "Sorry...
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            REQ_CODE_SPEECH_INPUT -> if (resultCode == RESULT_OK && data != null) {
+                get_string_voice_input =
+                    data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)!! // reusult1 được lấy giá trị mảng chuỗi kí tự nói vào
+                binding.etDescription.setText(get_string_voice_input[0]) // Hiển thị text ra màn hình
+            }
+        }
     }
 }
+
